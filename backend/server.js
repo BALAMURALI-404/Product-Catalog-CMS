d4e47da67be7aff5f53e0f3a6073beac2467f690
Balamurali B <bbalamurali2004@gmail.com>
2025-04-07 22:25:01 +0530

    update product in backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/products.route.js';
 
 dotenv.config();
 
@@ -9,39 +9,13 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/api/products', async (req,res) => {
-    const product=req.body;
-
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success: false, message: 'Please fill all the fields'});
-    }
-
-    const newProduct = new Product(product);
-    try{
-        await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    }catch(error){
-        console.error("Error in creating product:", error.message);
-        res.status(500).json({success: false, message: 'Server Error'});
-    }
-
-})
-
-app.delete("/api/products/:id", async (req, res) =>{
-    const {id} = req.params;
-    try{
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: 'Product deleted successfully'});
-    }catch(error){
-
-    }
-})
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     connectDB();
     console.log('server started at http://localhost:5000');
-})
+});
 
 
 //9xHH3uIduRE87Rhi
-//33.45
+//33.45 
